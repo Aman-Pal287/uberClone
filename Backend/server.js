a047b5f565f8a76d87fbc0c79e059e096e4e5be1
Aman-Pal287 <palaman9786@gmail.com>
2025-10-13 17:42:24 +0530

    Initialize backend with Express, MongoDB connection, and basic server setup
    - Created package.json with necessary dependencies (express, mongoose, cors, dotenv).
    - Implemented server.js to set up the Express server and connect to MongoDB.
    - Added app.js to configure middleware (JSON parsing and CORS).
    - Created db.js for MongoDB connection handling with error logging.

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const app = require("./src/app");
+const PORT = process.env.PORT || 3000;
+const connectToDB = require("./src/db/db");
+
+connectToDB();
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
